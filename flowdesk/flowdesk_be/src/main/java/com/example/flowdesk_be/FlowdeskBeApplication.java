b5e0531f7ab6bab803aa6d051b5d1781f20933e0
Quangntph50607 <quangntph50607@gmail.com>
2026-08-18 15:53:51 +0700

    first commit

---
+++ flowdesk/flowdesk_be/src/main/java/com/example/flowdesk_be/FlowdeskBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowdesk_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowdeskBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowdeskBeApplication.class, args);
+    }
+
+}
---
+++ flowdesk/flowdesk_be/src/test/java/com/example/flowdesk_be/FlowdeskBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowdesk_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowdeskBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
